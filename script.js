a03425ba34e6670f54d1eee9575249f1f015e0e8
FrancescoViterbo <franc.viterbo@gmail.com>
2020-10-22 12:30:54 +0200

    incominciato a scriptare la logica dei reply

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+/* Selettori */
+
 const chatMsg = document.getElementById("chat-msg");
 const chatHistory = document.getElementById("chat-history");
 const sendBtn = document.getElementById("send-btn");
 
 sendBtn.addEventListener("click", getMsg);
 
+/* Frase di Benvenuto */
+const welcomeMsg = "Ciao, sono AIbot, vuoi giocare?";
+appendAIMsg(welcomeMsg);
+
+/* Liste di messaggi previsti dall'utente */
+const rispostePositive = ["si", "ok", "va bene", "yes", "vai", "giusto"];
+const risposteNegative = ["no", "sbagliato"];
+const rispostePreviste = [rispostePositive, risposteNegative];
+
+/* Setto dei booleani per definire le fasi del gioco */
+
+/* Iterazione per messaggi ricevuti */
+function processaMessaggio(message){
+    for (let index = 0; index < rispostePreviste.length; index++) {
+        if(rispostePreviste[index].includes(message)){
+            rispondi(reply);
+        } else {
+            rispondi(noReply);
+        }
+    }
+}
+
+function rispondi(r){
+    if(r = reply){
+
+    }
+}
+
+/* FUNZIONI */
 function getMsg(){
     let message = chatMsg.value;
     appendUserMsg(message);
@@ -25,9 +56,10 @@ function appendUserMsg(message){
 }
 
 function AIreply(message){
-    if (userMessages.includes(message)){
+    // if (userMessages.includes(message)){
 
-    }
+    // }
+    appendAIMsg(reply);
 }
 
 function appendAIMsg(message){
